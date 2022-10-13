94922662326e00d3b51e250759c5714a08cfbe0c
Disconnector <88462140+McCoderWaitingForHytale@users.noreply.github.com>
2022-10-13 14:57:56 +0200

    Update recipes.lua

--- prototypes/recipes.lua
+++ prototypes/recipes.lua
@@ -2,7 +2,7 @@ data:extend({
 {
 type = "item",
 name = "verylonginserter",
-icon = "_steves-train-refuel_/graphics/icons/foobaricon.png",
+icon = "_steves-train-refuel_/graphics/icons/verylonginserter.png",
 flags = {"goes-to-quickbar"},
 subgroup = "ammo",
 stack_size = 100,
