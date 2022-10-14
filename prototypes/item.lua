fab30b55b00fb5ea189954672c55ec696c41c89e
Disconnector <88462140+McCoderWaitingForHytale@users.noreply.github.com>
2022-10-14 19:11:23 +0200

    Update item.lua

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -1,1 +1,12 @@
+data:extend({
 
+{
+type = "item",
+name = "foobar",
+icon = "better-transport/graphics/icons/yellow_belt.png",
+flags = {"goes-to-inventory"},
+subgroup = "logistic",
+stack_size = 100,
+},
+
+  })
