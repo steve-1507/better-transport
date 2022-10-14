2b264c0129bb48f13c792b104601fb04cac1e1a7
Disconnector <88462140+McCoderWaitingForHytale@users.noreply.github.com>
2022-10-14 20:51:07 +0200

    Update item.lua

--- prototypes/item.lua
+++ prototypes/item.lua
@@ -6,6 +6,7 @@ name = "yellow-transport-belt",
 icon = "better-transport/graphics/icons/yellow-transport-belt.png",
 subgroup = "belt",
 order = "a[transport-belt]-d[yellow-transport-belt]
+place_result = "yellow-transport-belt"
 stack_size = 100,
 },
 
