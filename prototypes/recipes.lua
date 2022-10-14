a71d74568d816f3ca646aad8cd139d7ddaae7361
Disconnector <88462140+McCoderWaitingForHytale@users.noreply.github.com>
2022-10-14 19:44:37 +0200

    Update and rename Recipes.Lua to recipes.lua

---
+++ prototypes/recipes.lua
@@ -0,0 +1,12 @@
+data:extend({
+{
+    type = "recipe",
+    name = "yellow-transport-belt",
+    enabled = "true",
+    ingredients = 
+    {
+      {"advanced-circuit",5},{"express-transport-belt",3}
+    },
+    result = "yellow-transport-belt"
+  },
+    )}
