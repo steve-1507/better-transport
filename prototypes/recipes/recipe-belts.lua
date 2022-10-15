c97a78da80440519c735b4c099586f3bdd1f85ad
Disconnector <88462140+McCoderWaitingForHytale@users.noreply.github.com>
2022-10-15 13:13:20 +0200

    Create recipe-belts.lua

---
+++ prototypes/recipes/recipe-belts.lua
@@ -0,0 +1,20 @@
+data:extend(
+{
+	{
+		type = "recipe",
+		name = "yellow-transport-belt",
+		category = "crafting",
+		enabled = false,
+		ingredients =
+		{
+			{"express-transport-belt", 3},
+			{"advanced-circuits", 5},
+			
+		},
+		result = "yellow-transport-belt",
+		result_count = 1
+	},
+   
+  
+  }
+)
