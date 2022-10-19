5fab15181fae5105a6068851d947ab0ebee41b9c
Disconnector <88462140+McCoderWaitingForHytale@users.noreply.github.com>
2022-10-19 13:46:45 +0200

    Update recipe-belts.lua

--- prototypes/recipes/recipe-belts.lua
+++ prototypes/recipes/recipe-belts.lua
@@ -6,12 +6,12 @@ data:extend(
 		ingredients =
 		{
 			{"express-transport-belt", 3},
-			{"advanced-circuits", 5},
+			{"advanced-circuits", 5}
 			
 		},
 		result = "yellow-transport-belt",
 		result_count = 2
-	},
+	}
    
   
   }
